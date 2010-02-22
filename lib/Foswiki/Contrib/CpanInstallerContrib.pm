8632dea6543afc03ece75698373e9148ef1fd83e
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-22 01:27:14 +0000

    Item8597: split from CpanContrib
    git-svn-id: http://svn.foswiki.org/trunk/CpanInstallerContrib@6468 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Contrib/CpanInstallerContrib.pm
@@ -0,0 +1,15 @@
+package Foswiki::Contrib::CpanInstallerContrib;
+
+use vars qw( $VERSION $RELEASE );
+# This should always be $Rev: 6467 $ so that Foswiki can determine the checked-in
+# status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+$VERSION = '$Rev: 6467 $';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+$RELEASE = '20100221';
+
+
+1;
