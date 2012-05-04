fb47f3c95d8277d45214893915c1467b7a746712
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/CpanInstallerContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/CpanInstallerContrib.pm
+++ lib/Foswiki/Contrib/CpanInstallerContrib.pm
@@ -1,6 +1,7 @@
 package Foswiki::Contrib::CpanInstallerContrib;
 
 use vars qw( $VERSION $RELEASE );
+
 # This should always be $Rev: 6467 $ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
@@ -10,6 +11,5 @@ $VERSION = '$Rev: 6467 $';
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = '20100221';
-
 
 1;
